1cb6feb6bc4c6e6dd244de9dbc6bcdeaa89329eb
Felipe Graeff <felipegraeff@hotmail.com>
2025-04-25 12:45:19 -0300

    reorganized files to react project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import hello from "./assets/images/hello.gif";
+import woah from "./assets/images/woah.gif";
+import floppy from "./assets/images/floppy.gif";
+import surftime from "./assets/images/surftime.gif";
+import note from "./assets/images/note.gif";
+import baby from "./assets/images/baby.gif";
+import enter from "./assets/images/enter.gif";
+import camera from "./assets/images/camera.gif";
+import image from "./assets/images/image.png";
+import cool from "./assets/images/cool.gif";
+import okay from "./assets/images/okay.gif";
+import keyboard from "./assets/images/keyboard.gif";
+import blown from "./assets/images/blown.gif";
+import run from "./assets/images/run.gif";
+import counter from "./assets/images/counter.gif";
+import back from "./assets/images/back.gif";
 
 export default function App() {
   return (
     <div
       style={{
-        backgroundImage: "url('/images/back.gif')",
+        backgroundImage: `url(${back})`,
         textAlign: "center",
         minHeight: "100vh",
       }}
@@ -17,23 +33,18 @@ export default function App() {
                 <tbody>
                   <tr>
                     <td>
-                      <img src="/images/hello.gif" alt="Hello GIF" />
+                      <img src={hello} alt="Hello GIF" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <img src={woah} alt="Woah GIF" height="50%" width="50%" />
                     </td>
                   </tr>
                   <tr>
                     <td>
                       <img
-                        src="/images/woah.gif"
-                        alt="Woah GIF"
-                        height="50%"
-                        width="50%"
-                      />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img
-                        src="/images/floppy.gif"
+                        src={floppy}
                         alt="Floppy GIF"
                         height="30%"
                         width="30%"
@@ -43,7 +54,7 @@ export default function App() {
                   <tr>
                     <td>
                       <img
-                        src="/images/surftime.gif"
+                        src={surftime}
                         alt="Surf Time GIF"
                         height="50%"
                         width="50%"
@@ -52,17 +63,12 @@ export default function App() {
                   </tr>
                   <tr>
                     <td>
-                      <img
-                        src="/images/note.gif"
-                        alt="Note GIF"
-                        height="20%"
-                        width="20%"
-                      />
+                      <img src={note} alt="Note GIF" height="20%" width="20%" />
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <img src="/images/baby.gif" alt="Baby GIF" />
+                      <img src={baby} alt="Baby GIF" />
                     </td>
                   </tr>
                 </tbody>
@@ -82,17 +88,17 @@ export default function App() {
                         Welcome to Felipe Graeff’s amazing website
                       </h1>
                       <a href="/software" target="_blank" rel="noreferrer">
-                        <img src="/images/enter.gif" alt="Enter Software" />
+                        <img src={enter} alt="Enter Software" />
                       </a>
                       <br />
                       <a href="/photo" target="_blank" rel="noreferrer">
-                        <img src="/images/camera.gif" alt="Enter Photo" />
+                        <img src={camera} alt="Enter Photo" />
                       </a>
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <img src="/images/image.png" alt="Placeholder" />
+                      <img src={image} alt="Placeholder" />
                     </td>
                   </tr>
                 </tbody>
@@ -104,7 +110,7 @@ export default function App() {
                   <tr>
                     <td>
                       <img
-                        src="/images/cool.gif"
+                        src={cool}
                         alt="Cool GIF"
                         height="100%"
                         width="100%"
@@ -113,18 +119,13 @@ export default function App() {
                   </tr>
                   <tr>
                     <td>
-                      <img
-                        src="/images/okay.gif"
-                        alt="Okay GIF"
-                        height="50%"
-                        width="50%"
-                      />
+                      <img src={okay} alt="Okay GIF" height="50%" width="50%" />
                     </td>
                   </tr>
                   <tr>
                     <td>
                       <img
-                        src="/images/keyboard.gif"
+                        src={keyboard}
                         alt="Keyboard GIF"
                         height="50%"
                         width="50%"
@@ -134,7 +135,7 @@ export default function App() {
                   <tr>
                     <td>
                       <img
-                        src="/images/blown.gif"
+                        src={blown}
                         alt="Blown GIF"
                         height="50%"
                         width="50%"
@@ -143,17 +144,12 @@ export default function App() {
                   </tr>
                   <tr>
                     <td>
-                      <img
-                        src="/images/run.gif"
-                        alt="Run GIF"
-                        height="10%"
-                        width="10%"
-                      />
+                      <img src={run} alt="Run GIF" height="10%" width="10%" />
                     </td>
                   </tr>
                   <tr>
                     <td>
-                      <img src="/images/counter.gif" alt="Counter GIF" />
+                      <img src={counter} alt="Counter GIF" />
                     </td>
                   </tr>
                 </tbody>
